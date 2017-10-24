7271de8f08eb7768e0433e032d145556454e7898
Mohammed Sayeed <mohammed.sayeed@ogilvy.com>
2017-10-24 18:27:25 +0400

    added compass process

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ var gulp = require('gulp')
 	gutil = require('gulp-util'),
 	coffee = require('gulp-coffee')
 	concat = require('gulp-concat')
-	browserify = require('gulp-browserify');
+	browserify = require('gulp-browserify')
+	compass = require('gulp-compass');
 
 /*
 gulp.task('log', function(){
@@ -18,6 +19,8 @@ var jsSources = [
 	'components/scripts/template.js'
 ];
 
+var sassSources = ['components/sass/style.scss'];
+
 gulp.task('coffee', function(){
 	gulp.src(coffeeSources)
 		.pipe(coffee({ bare: true })
@@ -30,4 +33,14 @@ gulp.task('js', function(){
 		.pipe(concat('script.js'))
 		.pipe(browserify())
 		.pipe(gulp.dest('builds/development/js'))
+});
+
+gulp.task('compass', function(){
+	gulp.src(sassSources)
+		.pipe(compass({
+			sass: 'components/sass',
+			image: 'builds/development/images',
+			style: 'expanded'
+		}).on('error', gutil.log))
+		.pipe(gulp.dest('builds/development/css'))
 });
